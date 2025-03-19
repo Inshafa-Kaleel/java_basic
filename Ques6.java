58c4cbfca4b65ac4a135c991b0699d65fdc94b89
Inshafa-Kaleel <154611829+Inshafa-Kaleel@users.noreply.github.com>
2025-03-19 18:14:37 +0000

    Create an ArrayList to store Student objects

---
+++ Ques6.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class Student {
+    String name;
+    int marks;
+
+    // Constructor to initialize Student object
+    public Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
