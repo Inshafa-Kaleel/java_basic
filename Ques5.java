d85053459be414a32aa7bd7cbd776a797231e18b
Inshafa-Kaleel <154611829+Inshafa-Kaleel@users.noreply.github.com>
2025-03-19 18:12:15 +0000

    create a student class

--- Ques5.java
+++ Ques5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Ques5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
