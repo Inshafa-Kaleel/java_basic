ea5dcabedf8d58d15902c6d3e774cd11685389a5
Inshafa-Kaleel <154611829+Inshafa-Kaleel@users.noreply.github.com>
2025-03-19 18:16:58 +0000

    Store Student Objects in an ArrayList

--- Ques6.java
+++ Ques6.java
@@ -10,3 +10,15 @@ class Student {
         this.marks = marks;
     }
 }
+
+public class Ques6 {
+    public static void main(String[] args) {
+       // Create an ArrayList to store Student objects
+        ArrayList<Student> students = new ArrayList<>();
+
+         // Add 5 Student objects to the list
+        students.add(new Student("inshafa", 85));
+        students.add(new Student("poos", 78));
+        students.add(new Student("akee", 90));
+        students.add(new Student("fahm", 88));
+        students.add(new Student("hamee", 76));
